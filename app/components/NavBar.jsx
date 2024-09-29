20c4ea9aa26921da211913aaf7c1734f010fb531
Brian Waweru <brianwaweru627@gmail.com>
2024-09-29 06:44:55 +0300

    Added link to Questionnaires list

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -29,7 +29,9 @@ export default function NavBar() {
                 <button
                     className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#1980e6] text-white text-sm font-bold leading-normal tracking-[0.015em]"
                 >
-                    <span className="truncate">Sign in</span>
+                    <a href="/questionnaires/list">
+                        <span className="truncate">Questionnaires</span>
+                    </a>
                 </button>
                 </div>
             </header>
